ddc759e884591c91b67d69345a35550ac0acb156
Jhonatta Santos <jhonattasantoss@hotmail.com>
2024-03-04 23:58:56 -0300

    Add ProtectedRoute component for authenticated routes

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -3,13 +3,18 @@ import Layout from './layouts/layout';
 import Home  from '@/pages/Home';
 import AuthCallback from './pages/AuthCallback';
 import UserProfile from './pages/UserProfile';
+import ProtectedRoute from './auth/ProtectedRoute';
 
 const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<Layout showHero><Home /></Layout>} / >
             <Route path="/auth-callback" element={<AuthCallback />} / >
-            <Route path="/user-profile" element={<Layout><UserProfile /></Layout>} / >
+                
+            <Route element={<ProtectedRoute />}>
+                <Route path="/user-profile" element={<Layout><UserProfile /></Layout>} / >
+            </Route>
+
             <Route path="/*" element={<Navigate to="/" />} />
         </Routes>
     );
